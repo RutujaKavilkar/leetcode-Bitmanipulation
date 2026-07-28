10a051f6f0711e852158f64931b04b20b8e0c22f
RutujaKavilkar <rutujabkavilkar19@gmail.com>
2026-07-28 18:55:25 +0530

    Added Toggle_Ith_Bit Solution

---
+++ Bitmanipulation/Toggle_Ith_Bit.java
@@ -0,0 +1,11 @@
+class Main {
+    public static int toggleIthBit(int num,int i)
+    {
+        return num ^ (1<<i);
+    }
+    public static void main(String[] args) {
+        int num=10;
+        int i=1;
+
+    }
+}
