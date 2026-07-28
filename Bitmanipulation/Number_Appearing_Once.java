ccc0c37ad6d0d507d07c56d8cf58ad0bfda673d1
RutujaKavilkar <rutujabkavilkar19@gmail.com>
2026-07-28 22:52:56 +0530

    Added Number_Appearing_Once Solution

---
+++ Bitmanipulation/Number_Appearing_Once.java
@@ -0,0 +1,17 @@
+class Main {
+    public static int numAppearingOnce(int arr[])
+    {
+        int xor=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            xor=xor^arr[i];
+        }
+        return xor;
+    }
+    public static void main(String[] args) {
+
+        int arr[] = {1,2,3,2,1};
+
+        System.out.println(numAppearingOnce(arr));
+    }
+}
